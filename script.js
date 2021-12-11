62288fe20a682a7fa0c60dc16977bced0e1d0d68
Shah Newaj Shishir <shahshishir.09@gmail.com>
2021-12-11 23:44:41 +0600

    add update user

--- script.js
+++ script.js
@@ -1,31 +1,65 @@
-      
 const name = document.querySelector("#fullName");
 const age = document.querySelector("#age");
 const form = document.querySelector("form");
 const submitBtn = document.querySelector("#submit-btn");
 const tbody = document.querySelector("#table tbody");
-const selectedUserShow=document.querySelector("#show #selected-user")
-let count = 1;
-form.addEventListener("submit", addUser);
+const selectedUserShow = document.querySelector("#show #selected-user");
 
+let count = 1;
+let submitAction = "add";
+let userSerial = -1;
+
+form.addEventListener("submit", submitForm);
 name.addEventListener("change", buttonEnable);
 age.addEventListener("change", buttonEnable);
 
 function buttonEnable() {
-    if (name.value !== "" && age.value !== "")
-        submitBtn.disabled = false;
-    else
-        submitBtn.disabled = true;
+  if (name.value !== "" && age.value !== "") {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
 }
 
-function addUser(e) {
+function clearSelectedUser() {
+  selectedUserShow.innerHTML = "";
+}
 
-    e.preventDefault();
-    clearSelectedUser();
-    const person = { name: name.value, age: age.value };
-    const tr = document.createElement("tr");
+function submitForm(e) {
+  e.preventDefault();
 
-    tr.innerHTML = `
+  if (submitAction === "add") {
+    addUser();
+  } else if (submitAction === "update") {
+    updateUser(e);
+  }
+}
+
+function getUser(e) {
+  const selectedParent = e.target.parentElement;
+  const selectedRow = selectedParent.parentElement;
+  const selectedName = selectedRow.children[0].innerHTML;
+  const selectedAge = selectedRow.children[1].innerHTML;
+  const selectedUserDiv = document.createElement("div");
+
+  selectedUserDiv.innerHTML = `
+          <h3>${selectedName}</h3>
+          <h3>${selectedAge}</h3>
+      `;
+
+  clearSelectedUser();
+
+  selectedUserShow.appendChild(selectedUserDiv);
+}
+
+function addUser() {
+  console.log(submitAction);
+  clearSelectedUser();
+
+  const person = { name: name.value, age: age.value };
+  const tr = document.createElement("tr");
+  tr.id = `user-${count}`;
+  tr.innerHTML = `
           <td>${person.name} </td>
           <td>${person.age}</td>
           <td>
@@ -34,66 +68,75 @@ function addUser(e) {
           <button id="delete-${count}">Delete</button>
           </td>
         `;
-    
-    tbody.appendChild(tr);
-    const selectedBtn = document.querySelector(`#select-${count}`);
-    selectedBtn.addEventListener("click", getUser);
-    const deleteBtn = document.querySelector(`#delete-${count}`);
-    deleteBtn.addEventListener("click", deleteUser);
-    const updateBtn = document.querySelector(`#update-${count}`);
-    updateBtn.addEventListener('click', updateUser);
-    ++count;
-    form.reset();
-    buttonEnable()
-    
-    if (tbody.childElementCount > 0)
-        document.getElementById("show").style.display = "block";
-    else
-        document.getElementById("show").style.display = "none";
-    
-    
+  tbody.appendChild(tr);
+
+  const selectedBtn = document.querySelector(`#select-${count}`);
+  selectedBtn.addEventListener("click", getUser);
+
+  const deleteBtn = document.querySelector(`#delete-${count}`);
+  deleteBtn.addEventListener("click", deleteUser);
+
+  const updateBtn = document.querySelector(`#update-${count}`);
+  updateBtn.addEventListener("click", setUpdateForm);
+
+  ++count;
+
+  form.reset();
+  submitBtn.disabled = true;
+
+  if (tbody.childElementCount > 0) {
+    document.getElementById("show").style.display = "block";
+  } else {
+    document.getElementById("show").style.display = "none";
+  }
 }
 
-function clearSelectedUser() {
-    selectedUserShow.innerHTML = "";
+function setUpdateForm(e) {
+  clearSelectedUser();
+
+  const btnParent = e.target.parentElement;
+  const userRow = btnParent.parentElement;
+  userSerial = +userRow.id.split("-")[1];
+  const userName = userRow.children[0].innerText;
+  const userAge = userRow.children[1].innerText;
+
+  name.value = userName;
+  age.value = userAge;
+
+  submitBtn.disabled = false;
+
+  submitAction = "update";
+
+  console.log(userSerial);
 }
 
 function updateUser(e) {
+  console.log(submitAction);
+  const updatedName = name.value;
+  const updatedAge = age.value;
 
-    clearSelectedUser();
-    const btnParent = e.target.parentElement;
-    const userRow = btnParent.parentElement;
-    console.log(userRow);
-}
+  console.log(userSerial);
+  const updatedRow = document.querySelector(`#user-${userSerial}`);
 
-function getUser(e) {
-    
-    const selectedParent = e.target.parentElement;
-    const selectedRow = selectedParent.parentElement;
-    
-    const selectedName = selectedRow.children[0].innerHTML;
-    const selectedAge = selectedRow.children[1].innerHTML;
-    const selectedUserDiv = document.createElement("div");
-    selectedUserDiv.innerHTML = `
-        <h3>${selectedName}</h3>
-        <h3>${selectedAge}</h3>
-    `;
-    clearSelectedUser();
-    selectedUserShow.appendChild(selectedUserDiv)
-    
+  updatedRow.children[0].innerText = updatedName;
+  updatedRow.children[1].innerText = updatedAge;
+
+  form.reset();
+  submitBtn.disabled = true;
+  submitAction = "add";
+  console.log(updatedRow);
 }
 
 function deleteUser(e) {
+  clearSelectedUser();
 
-    clearSelectedUser();
-    const btnParent = e.target.parentElement;
-    const userRow = btnParent.parentElement;
-    userRow.remove();
-    if (tbody.childElementCount > 0)
-        document.getElementById("show").style.display = "block";
-    else
-        document.getElementById("show").style.display = "none";
-    // const tBody = document.querySelector("#table #user-rows-body")
-    // console.log(tBody);
-    // tBody.removeChild(userRow);
+  const btnParent = e.target.parentElement;
+  const userRow = btnParent.parentElement;
+  userRow.remove();
+
+  if (tbody.childElementCount > 0) {
+    document.getElementById("show").style.display = "block";
+  } else {
+    document.getElementById("show").style.display = "none";
+  }
 }
